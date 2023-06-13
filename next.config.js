fd85edd64d009ed75103215121340b1ea59fce4b
Relchzen <104548363+Relchzen@users.noreply.github.com>
2023-06-13 11:38:45 +0700

    reconfig next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
     distDir: 'build',
   swcMinify: true,
   images: {
